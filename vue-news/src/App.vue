06187245b112e53d03194bbc2a9ef121184aebf8
jiYoung4868 <orinogcode@gmail.com>
2023-12-19 17:25:10 +0900

    [jy] toolbar 스타일링

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
     <!--url이 만약 news라면 아래에 NewsView로 태그 생성-->
+ <ToolBar></ToolBar>
 <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue';
+
 
 export default {
+    components: { ToolBar }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  padding: 0;
+  margin: 0;
 }
 </style>

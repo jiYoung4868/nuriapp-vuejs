3f32ef68453d708dbd2c5d13bc9034f920711fbd
jiYoung4868 <orinogcode@gmail.com>
2023-12-19 17:32:39 +0900

    [jy] 과제

--- vue-news/src/routes/index.js
+++ vue-news/src/routes/index.js
@@ -3,10 +3,13 @@ import Vue from 'vue';
 import NewsView from '../views/NewsView.vue';
 import JobsView from '../views/JobsView.vue';
 import AskView from '../views/AskView.vue';
+import UserView from '../views/UserView.vue';
+import ItemView from '../views/ItemView.vue';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+    mode: 'history',
     routes: [
         {
             //path: url 주소
@@ -22,6 +25,14 @@ export const router = new VueRouter({
         {
             path: '/jobs',
             component: JobsView,
+        },
+        {
+            path: '/user',
+            component: UserView,
+        },
+        {
+            path: '/item',
+            component: ItemView,
         }
     ]
 });
